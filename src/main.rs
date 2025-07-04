04b6073e187bd6db3da377ae797ff6abfe73603d
Cristian Gonzalez <alexsiscg@gmail.com>
2025-07-04 14:27:59 -0600

    completed the last problem from the chapter8

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io};
 
 fn main() {
     let mut v = vec![8, 8, 9, 2, 234, 10, 234, 81, 3, 3, 3];
@@ -28,4 +28,29 @@ fn main() {
     // let finals = format!("-{c1}ay");
     // s.push_str(&finals);
     println!("{}", format_args!("{s}-{c1}ay"));
+
+    interface_add_employee();
+}
+
+fn interface_add_employee() {
+    println!(
+        "Please insert the command as follows \"Add <Name of employee> to <name of the department>\""
+    );
+
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("Failed to read line");
+
+    let s_split: Vec<&str> = s.split_whitespace().collect();
+    let mut h = HashMap::new();
+
+    if s_split.len() >= 4 && s_split[0] == "Add" {
+        println!("{}", s_split[0]);
+        h.entry(s_split[1]).or_insert(match s_split.get(3) {
+            Some(x) => x,
+            None => return,
+        });
+    } else {
+        println!("Please insert the command given as above with the correct spaces");
+    }
+    println!("{:?}", h);
 }
